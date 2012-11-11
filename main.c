20a60ee6c8d3c74e7bfeeaea660bc355c504029d
RinaYaya <yacine.oleemahomed@club-internet.fr>
2012-11-11 17:54:36 +0100

    Nbetape et Colonne ok, test Alignhoriz

--- main.c
+++ main.c
@@ -11,13 +11,12 @@ p=NouveauP();
 p=AjouterPion(p,1,1);
 p=AjouterPion(p,2,1);
 p=AjouterPion(p,3,1);
-p=AjouterPion(p,4,1);
 p=AjouterPion(p,5,1);
 p=AjouterPion(p,6,1);
 p=AjouterPion(p,7,1);
 
 printf("\n derniere col joué : %d \n",Colonne(p));
 printf("\n nbre etape : %d \n",NbEtape(p));
-
+printf("\n Align Horiz %d",AlignHoriz(p,4));
 	return 0;
 }
