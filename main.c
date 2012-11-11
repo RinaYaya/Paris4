95eed473da0ee6b88d5a810587b759576e2c2ca7
RinaYaya <yacine.oleemahomed@club-internet.fr>
2012-11-11 19:16:12 +0100

    test aligvert

--- main.c
+++ main.c
@@ -14,8 +14,8 @@ p=AjouterPion(p,5,1);
 p=AjouterPion(p,6,1);
 p=AjouterPion(p,6,1);
 p=AjouterPion(p,7,1);
-
-
-printf("\n Align Horiz %d \n",AlignHoriz(p,5));
+printf("\n Align Vert %d \n",AlignVert(p,4));
+printf("\n Align Vert %d \n",AlignVert(p,5));
+printf("\n Align Vert %d \n",AlignVert(p,6));
 	return 0;
 }
