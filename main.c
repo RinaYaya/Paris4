8d22c8fb5b721571c8b734c19f5c6e749f306c25
RinaYaya <yacine.oleemahomed@club-internet.fr>
2012-11-11 17:43:49 +0100

    RE test Ajoutpion

--- main.c
+++ main.c
@@ -7,9 +7,14 @@ int main()
 int i;
 P4 p;
 p=NouveauP();
-printf("\n %d \n",p->p1.tete);
+
 p=AjouterPion(p,1,1);
-printf("\n %d \n",p->p1.tete);
+p=AjouterPion(p,2,1);
+p=AjouterPion(p,3,1);
+p=AjouterPion(p,4,1);
+p=AjouterPion(p,5,1);
+p=AjouterPion(p,6,1);
+p=AjouterPion(p,7,1);
 
 	return 0;
 }
